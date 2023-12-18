7592d3a9622b06b7c0fc04e2ce8feef9b26c682a
Q531219 <korbinian.zormeier@bmw.de>
2023-12-18 15:53:24 +0100

    DaySeven: Setup

---
+++ y2023_Java/src/day_07/DaySeven.java
@@ -0,0 +1,21 @@
+package day_07;
+
+import _general.Day;
+import day_06.DaySix;
+
+public class DaySeven extends Day {
+    public static void main(String[] args) {
+        DaySeven daySeven = new DaySeven();
+        System.out.println("Part I result: " + daySeven.partOne());
+        System.out.println("Part II result: " + daySeven.partTwo());
+    }
+    @Override
+    public int partOne() {
+        return 0;
+    }
+
+    @Override
+    public int partTwo() {
+        return 0;
+    }
+}
