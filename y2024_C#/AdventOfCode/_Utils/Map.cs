bf806dfce98c05b55249156da381d1576fd82cb6
k_zormeier <Korbinian.Zormeier@fast-forest.de>
2024-12-08 14:56:28 +0100

    2024-##: add Vector class to utils

--- y2024_C#/AdventOfCode/_Utils/Map.cs
+++ y2024_C#/AdventOfCode/_Utils/Map.cs
@@ -2,11 +2,11 @@ namespace AdventOfCode._Utils;
 
 public abstract class Map(List<string> data) {
     protected readonly List<string> Data = data;
-    
+
     public char Get(Position position) {
         return Data[position.Row][position.Column];
     }
-    
+
     public List<Position> GetAll(char letter) {
         var positions = new List<Position>();
         for (var row = 0; row < Data.Count; row++) {
@@ -20,7 +20,7 @@ public abstract class Map(List<string> data) {
 
         return positions;
     }
-    
+
     public bool InsideOfMap(Position position) {
         var rowWithinBounds = 0 <= position.Row && position.Row < Data.Count;
         if (rowWithinBounds) {
@@ -32,4 +32,24 @@ public abstract class Map(List<string> data) {
     }
 }
 
-public record Position(int Row, int Column);
+public record Position(int Row, int Column) {
+    public Position Added(Vector vector) {
+        var newRow = Row + vector.Rows;
+        var newColumn = Column + vector.Columns;
+        return new Position(newRow, newColumn);
+    }
+};
+
+public record Vector(int Rows, int Columns) {
+    public static Vector FromPositions(Position tail, Position head) {
+        var deltaRow = head.Row - tail.Row;
+        var deltaColumn = head.Column - tail.Column;
+        return new Vector(deltaRow, deltaColumn);
+    }
+
+    public Vector MultipliedBy(int scalar) {
+        var newRows = Rows * scalar;
+        var newColumns = Columns * scalar;
+        return new Vector(newRows, newColumns);
+    }
+};
